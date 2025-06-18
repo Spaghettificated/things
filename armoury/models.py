c47c081040d5d3c4512f197ca717e78825f20adc
Eli <lescza@st..amu.edu.pl>
2025-06-18 02:33:56 +0200

    tweak database and names

--- armoury/models.py
+++ armoury/models.py
@@ -3,23 +3,32 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=30)
-    describtion = models.TextField()
-    creation_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    desc = models.TextField("describtion")
+    created = models.DateTimeField("creation_date", auto_now=False, auto_now_add=False)
     is_deleted = models.BooleanField()
     # creator_user
-    image_url = models.CharField(max_length=100, )
+    image_url = models.CharField(max_length=100, null = True)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    description = models.TextField()
-    creation_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    desc = models.TextField("describtion")
+    created = models.DateTimeField("creation_date", auto_now=False, auto_now_add=False)
     is_deleted = models.BooleanField()
     #creator_user
+
+    def __str__(self):
+        return f"[{self.product}] {self.desc[:30]}{"" if len(self.desc)<30 else "..."}"
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
     products = models.ManyToManyField(Product)
     is_deleted = models.BooleanField()
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null = True)
+
+    def __str__(self):
+        return self.name
 
