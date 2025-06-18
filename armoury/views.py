89d80fae941e02b6db438a33f65b7a1c90e12a6c
Eli <lescza@st..amu.edu.pl>
2025-06-18 07:32:24 +0200

    template views

--- armoury/views.py
+++ armoury/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Comment, Category
 
 
 def product_list(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    products = get_list_or_404(Product, is_deleted=False)
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "list.html", {"products": products})
 
 def product_page(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    return HttpResponse(f"product #{product_id}: {product.title}")
+    product = get_object_or_404(Product, pk=product_id, is_deleted=False)
+    comments = list(Comment.objects.all().filter(product=product))
+    # return HttpResponse(f"product #{product_id}: {product.title}")
+    return render(request, "product.html", {"product": product, "comments": comments})
         
