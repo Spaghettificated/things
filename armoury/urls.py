7e251761282bc742c546a6ee5cf707ca40a60507
Eli <lescza@st..amu.edu.pl>
2025-06-18 06:46:42 +0200

    basic product view

--- armoury/urls.py
+++ armoury/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.product_list, name="list"),
+    path("<int:product_id>/", views.product_page, name="product"),
 ]
--- armoury/views.py
+++ armoury/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product
 
 
-def index(request):
+def product_list(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def product_page(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return HttpResponse(f"product #{product_id}: {product.title}")
+        
