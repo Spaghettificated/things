8a774d32aae33063a6ad53d974ab735a9271274c
Eli <lescza@st..amu.edu.pl>
2025-06-18 02:01:55 +0200

    django extensions for better shell experience (shell_plus)

--- productify/settings.py
+++ productify/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'armoury.apps.ArmouryConfig',
+    'django_extensions',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
